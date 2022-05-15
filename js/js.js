594e855f3c50714c228cac791a08f602892b0db4
DaniloCaro <94326335+DaniloCaro@users.noreply.github.com>
2022-05-15 18:06:42 -0500

    Commit Uno

---
+++ js/js.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    document.querySelector("#title").classList.toggle("dark-mode__title");
+    // document.querySelector(".button--blue").style.backgroundColor = "white";
+}
