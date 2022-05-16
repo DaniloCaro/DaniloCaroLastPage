62944fcf3d3f9a402c6f1d9bbc8d71b8f739fc22
DaniloCaro <94326335+DaniloCaro@users.noreply.github.com>
2022-05-15 22:02:28 -0500

    Commit dos

--- js/js.js
+++ js/js.js
@@ -1,6 +1,18 @@
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
+
     document.querySelector("#title").classList.toggle("dark-mode__title");
-    // document.querySelector(".button--blue").style.backgroundColor = "white";
+
+    var element = document.querySelector(".button--blue");
+    element.classList.toggle("bg--white");
+
+    var element = document.querySelector(".briefcase-skills__buttons ");
+    element.classList.toggle("bg--white");
+
+    var element = document.querySelector(".buttons__b-s");
+    element.classList.toggle("bg--grey");    
+    
+    var element = document.querySelector(".skills");
+    element.classList.toggle("bg--white");   
 }
