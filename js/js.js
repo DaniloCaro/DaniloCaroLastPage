cfe9aabb928860b2fd33210e4ee5876e71f53096
DaniloCaro <94326335+DaniloCaro@users.noreply.github.com>
2022-05-28 19:37:34 -0500

    Funcionalidad con js de menú portafolio y skills

--- js/js.js
+++ js/js.js
@@ -10,9 +10,41 @@ function myFunction() {
     element = document.querySelector(".briefcase-skills__buttons ");
     element.classList.toggle("bg--white");
 
-    element = document.querySelector(".buttons__b-s");
-    element.classList.toggle("bg--grey");    
-    
-    element = document.querySelector(".skills");
-    element.classList.toggle("bg--white");   
+    // element = document.querySelector(".buttons__b-s");
+    // element.classList.toggle("bg--grey");    
 }
+
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
+const content1 = document.getElementById("content1");
+const content2 = document.getElementById("content2");
+
+let choise = 1
+
+const changeOption = () => {
+    choise == 1 ? (
+        option1.classList.value = 'buttons__b-s activate' ,
+        content1.classList.value = 'cards-box activate-content'
+    )
+    : (
+        option1.classList.value = 'buttons__b-s' ,
+        content1.classList.value = 'cards-box'
+    )
+    choise == 2 ? (
+        option2.classList.value = 'buttons__b-s activate' ,
+        content2.classList.value = 'cards-box activate-content'
+    )
+    : (
+        option2.classList.value = 'buttons__b-s' ,
+        content2.classList.value = 'cards-box'
+    )
+}
+
+option1.addEventListener('click', () => {
+    choise = 1
+    changeOption()
+})
+option2.addEventListener('click', () => {
+    choise = 2
+    changeOption()
+})
